c6836142bce19948ef3eb8c16f0fff970ccbf54d
小小小寒 <10341459@qq.com>
2020-12-13 23:49:46 +0800

    修复路由版本问题

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import {Route, Switch, BrowserRouter, Router } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Router } from 'react-router-dom';
 import Register from './containers/register';
 import Login from './containers/login';
 import PersonalInfoCon from './containers/personalInfo/personalInfo.jsx';
@@ -10,21 +10,25 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './assets/index.less'
 import history from './utils/history'
-import { createBrowserHistory } from 'history'
-import {} from 'react-router'
+// import { createBrowserHistory } from 'history'
+
+// const history = createBrowserHistory()
 
 // 测试提交hahahh
 ReactDom.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route path='/login' component={Login} />
-                <Route path='/register' component={Register} />
-
-                <Route path='/personalInfo'>
-                    <PersonalInfoCon></PersonalInfoCon>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'))
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route path='/login'>
+          <Login key='login'></Login>
+        </Route>
+        <Route path='/register'>
+          <Register key='register'></Register>
+        </Route>
+        <Route path='/personalInfo'>
+          <PersonalInfoCon key='personalInfo'></PersonalInfoCon>
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
+  , document.getElementById('root'))
